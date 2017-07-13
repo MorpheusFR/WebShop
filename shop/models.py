c5ac798efb10d4aa60998d242fb63637fbca43f0
MorpheusFR <Creatorsk@gmail.com>
2017-07-13 16:45:55 +0300

    Add models orders and Customers

--- shop/models.py
+++ shop/models.py
@@ -65,3 +65,70 @@ class FeedBack(models.Model):
 class Customer(models.Model):
     user = models.OneToOneField(User)
     pass
+
+    user = models.OneToOneField(User)
+    user_name = models.CharField(max_length=200, default='', db_index=True, verbose_name="Name")
+
+    #def __unicode__(self):
+    #    return self.user
+
+
+    # first_name = models.CharField(max_length=50, default=True, verbose_name='Имя')
+    # last_name = models.CharField(max_length=50, default=True, verbose_name='Фамилия')
+    # password = models.CharField(max_length=100, default=True)
+    # phone = models.CharField(max_length=10, default=True, verbose_name='Телефон')
+    # email = models.EmailField(default=True)
+    # date_of_birth = models.DateField(default=True, verbose_name='Дата рождения')
+
+    avatar = models.ImageField(upload_to='customer_avatar', blank=True, null=True, verbose_name="Avatar")
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True, verbose_name="Создан")
+    updated = models.DateTimeField(auto_now=True, blank=True, null=True, verbose_name="Обновлено")
+
+    #orders =
+    #reviews =
+    #wishes =
+
+
+
+    def __str__(self):
+        return self.user_name
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
+
+class Orders(models.Model):
+
+    name = models.CharField(max_length=200, db_index=True, verbose_name="Заказы")
+    quantity = models.PositiveIntegerField(verbose_name="Колличество")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Создан")
+    done = models.BooleanField(default=True, verbose_name="Выполнен")
+    canceled = models.BooleanField(default=True, verbose_name="Отменен")
+    orders = models.ForeignKey(Customer)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+
+class Reviews(models.Model):
+
+    name = models.CharField(max_length=200, db_index=True, verbose_name="Отзывы")
+    product = models.ManyToManyField(Product)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Создан")
+    caption = models.CharField(max_length=200, db_index=True, verbose_name="Заголовок")
+    text = models.TextField(blank=True, verbose_name="Текст отзыва")
+    reviews = models.ForeignKey(Customer)
+
+    stars = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
